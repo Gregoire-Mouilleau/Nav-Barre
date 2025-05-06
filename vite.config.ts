f2c095e280e941ddeebb8af3ff2fd72f5b570d05
Grégoire <gregoire.mouilleau@gmail.com>
2025-05-06 18:02:21 +0200

    bugfix publish versel

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,29 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+import * as path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
+    alias: [
+      { find: '@', replacement: path.resolve(__dirname, 'src') }
+    ]
   },
   build: {
     outDir: 'dist',
     emptyOutDir: true,
-    minify: 'esbuild'
-  }
+    minify: 'esbuild',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[hash][ext]'
+      }
+    }
+  },
+  publicDir: 'public'
 });
